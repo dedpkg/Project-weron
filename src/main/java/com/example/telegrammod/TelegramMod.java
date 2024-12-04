a3e14efb2e58d45bf202f892c9ce795f7542ee17
dedpkg <angelademianova665@gmail.com>
2024-12-05 00:41:03 +0300

    Update TelegramMod.java

--- src/main/java/com/example/telegrammod/TelegramMod.java
+++ src/main/java/com/example/telegrammod/TelegramMod.java
@@ -1,28 +1,34 @@
 package com.example.telegrammod;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.EventBus;
-import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraftforge.fml.common.event.FMLClientSetupEvent;
+import net.minecraftforge.fml.common.event.FMLCommonSetupEvent;
 
 @Mod.EventBusSubscriber(modid = TelegramMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
-public class TelegramMod {
+public class TelegramMod
+{
+    // Это строка будет использоваться для идентификации мода в файлах конфигурации.
     public static final String MODID = "telegrammod";
-    private static final Minecraft mc = Minecraft.getInstance();
+    // Логгер для мода
+    private static final org.apache.logging.log4j.Logger LOGGER = org.apache.logging.log4j.LogManager.getLogger();
 
-    // Этот метод будет вызван для инициализации мода
+    // Пример настройки клиента
     @SubscribeEvent
-    public static void onPlayerLogin(PlayerEvent.PlayerLoggedInEvent event) {
-        // Например, можно подключить Telegram API для уведомлений при входе игрока
-        System.out.println("Player " + event.getPlayer().getName().getString() + " has logged in.");
+    public static void onClientSetup(FMLClientSetupEvent event)
+    {
+        // Инициализация GUI
+        LOGGER.info("Телеграмм мод для Minecraft загружен!");
     }
 
-    public static void init() {
-        System.out.println("Initializing Telegram Mod");
-        // Подключаемся к Telegram API, инициализируем бота и т.д.
+    // Это будет вызвано на старте клиента для авторизации
+    public static void startLoginScreen() {
+        Minecraft.getMinecraft().displayGuiScreen(new TelegramLoginGui());
     }
 }
